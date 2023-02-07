e3118aa48b5d78890cecba19ddf929cc4fb6cfbc
Nickolas <89612417+lothllann@users.noreply.github.com>
2023-02-06 22:44:23 -0300

    criando os componentes

--- WEB/src/App.tsx
+++ WEB/src/App.tsx
@@ -1,9 +1,28 @@
 import './App.css'
+import { Home } from './components/home/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Menu from './components/menu/Menu';
+import Contato from './components/contato/Contato';
+import Portfolio from './components/portfolio/Portfolio';
+import Skills from './components/skills/Skills';
+import Sobre from './components/sobre/Sobre';
+
 
 function App() {
 
 return(
-<div className="teste">teste</div>
+<div className="app">
+  <BrowserRouter>
+    <Menu />
+    <main className='appBody'>
+      <Route path='/' element={<Home />} />
+      <Route path='/port' element={<Portfolio />} />
+      <Route path='/sobre' element={<Sobre />} />
+      <Route path='/contato' element={<Contato />} />
+      <Route path='/skills' element={<Skills />} />
+    </main>
+  </BrowserRouter>
+</div>
 )
 }
 
