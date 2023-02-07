1e114a4dc7aad43207b4436da8461bdd88ad9833
Nickolas <89612417+lothllann@users.noreply.github.com>
2023-02-06 23:04:58 -0300

    tailwind config

--- WEB/src/App.tsx
+++ WEB/src/App.tsx
@@ -1,29 +1,30 @@
-import './App.css'
-import { Home } from './components/home/Home'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Menu from './components/menu/Menu';
-import Contato from './components/contato/Contato';
-import Portfolio from './components/portfolio/Portfolio';
-import Skills from './components/skills/Skills';
-import Sobre from './components/sobre/Sobre';
-
+import "./App.css";
+import "./styles/global.css";
+import { Home } from "./components/home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Menu from "./components/menu/Menu";
+import Contato from "./components/contato/Contato";
+import Portfolio from "./components/portfolio/Portfolio";
+import Skills from "./components/skills/Skills";
+import Sobre from "./components/sobre/Sobre";
 
 function App() {
-
-return(
-<div className="app">
-  <BrowserRouter>
-    <Menu />
-    <main className='appBody'>
-      <Route path='/' element={<Home />} />
-      <Route path='/port' element={<Portfolio />} />
-      <Route path='/sobre' element={<Sobre />} />
-      <Route path='/contato' element={<Contato />} />
-      <Route path='/skills' element={<Skills />} />
-    </main>
-  </BrowserRouter>
-</div>
-)
+  return (
+    <div>
+      <BrowserRouter>
+        <Menu />
+        <main className="appBody">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/port" element={<Portfolio />} />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/contato" element={<Contato />} />
+            <Route path="/skills" element={<Skills />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- WEB/tailwind.config.cjs
+++ WEB/tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.tsx',
+    './index.html',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        background: '#1C1C1C'
+      }
+    },
   },
   plugins: [],
 }
