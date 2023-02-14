65532b52bb81ca5ebf735a2d3fcaa76ad5e949d8
lothllann <nick.ruppenthal@gmail.com>
2023-02-14 09:51:17 -0300

    home

--- WEB/src/components/home/Home.tsx
+++ WEB/src/components/home/Home.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Head from '../helper/Head'
+import './Home.css'
 
 export const Home = () => {
   return (
-    <section>
+    <>
     <Head title='Home' description='Home do meu portfólio pessoal' />
 
     <section className='mainHome'>
       <h1 className='TitleH1'>Hello There!</h1>
       <div className='MyNameHome' >
-        Meu nome é <span>Nickolas Ruppenthal,</span>
+        Meu nome é <span>Nickolas Ruppenthal</span>.
       </div>
       <div className='aboutHome'>
       Eu sou Web Developer,  muito bem vindo
@@ -17,6 +18,6 @@ export const Home = () => {
       meus projetos. E que a força esteja com voce
       </div>
     </section>
-  </section>
+  </>
   )
 }
