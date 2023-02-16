4979936465f4db9848ce67e6715f0773adfd5783
Nickolas <89612417+lothllann@users.noreply.github.com>
2023-02-16 19:09:50 -0300

    links

--- WEB/src/components/sobre/Sobre.tsx
+++ WEB/src/components/sobre/Sobre.tsx
@@ -1,71 +1,85 @@
-import React from 'react'
-import './Sobre.css'
-import photo from '../../assets/myPhoto.svg';
+import React from "react";
+import "./Sobre.css";
+import photo from "../../assets/myPhoto.svg";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import EmailIcon from "@mui/icons-material/Email";
 
 const listOfSocialMedias = [
   {
-    name: 'GitHub',
-    image: 'img',
-    link: 'https://github.com/lothllann',
-    color: 'grey'
+    name: "GitHub",
+    image: <GitHubIcon sx={{ fontSize: 50, color: '#1C1C1C' }} />,
+    link: "https://github.com/lothllann",
+    color: "#BABBBD",
   },
   {
-    name: 'Linkedin',
-    image: 'img',
-    link: 'https://www.linkedin.com/in/nickolas-ruppenthal-38b2ba167/',
-    color: 'grey'
+    name: "Linkedin",
+    image: <LinkedInIcon sx={{ fontSize: 50, color: '#1C1C1C' }} />,
+    link: "https://www.linkedin.com/in/nickolas-ruppenthal-38b2ba167/",
+    color: "#0A66C2",
   },
   {
-    name: 'Instagram',
-    image: 'img',
-    link: 'https://www.instagram.com/ruupp___/',
-    color: 'grey'
+    name: "Instagram",
+    image: <InstagramIcon sx={{ fontSize: 50, color: '#1C1C1C' }} />,
+    link: "https://www.instagram.com/ruupp___/",
+    color: "#E4405F",
   },
   {
-    name: 'Gmail',
-    image: 'img',
-    link: 'https://github.com/lothllann',
-    color: 'grey'
+    name: "Gmail",
+    image: <EmailIcon sx={{ fontSize: 50, color: '#1C1C1C' }} />,
+    link: "https://github.com/lothllann",
+    color: "#D14836",
   },
   {
-    name: 'WhatsApp',
-    image: 'img',
-    link: 'https://wa.me/5554991807117',
-    color: 'grey'
-  }
-]
+    name: "WhatsApp",
+    image: <WhatsAppIcon sx={{ fontSize: 50, color: '#1C1C1C' }} />,
+    link: "https://wa.me/5554991807117",
+    color: "#25D366",
+  },
+];
 
 const Sobre = () => {
-  const [open, setOPen] = React.useState(false)
+  const [open, setOPen] = React.useState(false);
 
-  const handleToggle = () =>{
-    setOPen(!open)
-
-  }
+  const handleToggle = () => {
+    setOPen(!open);
+  };
   return (
-    <section className='mainAbout'>
-      <div className={`${open ? 'active': ''} card`}>
-        <div className='user'>
-          <div className='img'>
-              <img src={photo} alt="minha foto de perfil" />
+    <section className="mainAbout">
+      <div className={`${open ? "active " : ""}card`}>
+        <div className="user">
+          <div className="img">
+            <img src={photo} alt="minha foto de perfil" />
           </div>
           <div className="content">
-            <h2>Nickolas Ruppenthal<br/><span>Web Developer</span></h2>
+            <h2>
+              Nickolas Ruppenthal
+              <br />
+              <span>Web Developer</span>
+            </h2>
           </div>
-          <button className='toggle' onClick={handleToggle}>{`${open ? 'Close': 'Click Here'}`}</button>
+          <button className="toggle" onClick={handleToggle}>{`${
+            open ? "Close" : "Click Here"
+          }`}</button>
         </div>
+
+        {open ? (
+        <div className="containerSocialMedia">
+          {listOfSocialMedias.map((socialMedia, i) => (
+            <a key={`${socialMedia.name}-${i}`} href={socialMedia.link} className="socialMediaCard">
+              <div className="sociallMediaImg">{socialMedia.image}</div>
+              <span className="sociallMediaText">{socialMedia.name}</span>
+            </a>
+          ))}
+        </div>
+      ): null}
       </div>
 
-       {listOfSocialMedias.map((SocialMedia, i)=>{
-          return(console.log(SocialMedia.name))
-        })}
-   
+      
     </section>
-  )
-}
+  );
+};
 
-export default Sobre
-
-
-
-
+export default Sobre;
