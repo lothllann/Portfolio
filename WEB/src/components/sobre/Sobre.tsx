b447e926e98e4dad18fd04dfe924e20963892135
Nickolas <89612417+lothllann@users.noreply.github.com>
2023-02-20 19:30:11 -0300

    sobre
    finalizando a pagina de sobre

--- WEB/src/components/sobre/Sobre.tsx
+++ WEB/src/components/sobre/Sobre.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Sobre.css";
+import "../../App.css";
 import photo from "../../assets/myPhoto.svg";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import EmailIcon from "@mui/icons-material/Email";
-import Head from '../helper/Head'
+import Head from "../helper/Head";
 
 const listOfSocialMedias = [
   {
@@ -49,10 +50,21 @@ const Sobre = () => {
   };
   return (
     <section className="mainAbout">
-      <Head title='Sobre' description='página que fala um pouco sobre mim' />
+      <Head title="Sobre" description="página que fala um pouco sobre mim" />
 
       <div className="about">
-        teste
+        <h1>Sobre Mim</h1>
+        <div>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </div>
       </div>
 
       <div className={`${open ? "active " : ""}card`}>
