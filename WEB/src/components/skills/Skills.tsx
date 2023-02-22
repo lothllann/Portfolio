5affa0371bef82a5295b926eba3a1a52801e0be2
lothllann <nick.ruppenthal@gmail.com>
2023-02-22 09:26:14 -0300

    skills

--- WEB/src/components/skills/Skills.tsx
+++ WEB/src/components/skills/Skills.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Head from "../helper/Head";
 import "./Skills.css";
 import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded'; //seta menor para baixo
-import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded'; //seta maior para a direita
+
 
 const Skills = () => {
   return (
@@ -13,16 +13,21 @@ const Skills = () => {
       />
       <section className="mainSkills">
         <div className="introSkills">
-          <h1 className="text-4xl font-extrabold mb-5">Skills</h1>
           <div className="apresentacao">
+          <h1 className="text-4xl font-extrabold mb-5">Skills</h1>
+
+            <div>
             Como eu curto mais o <span>Front-End</span> as minhas habilidades
             estão mais concentradas nessa area, porém eu pretendo evoluir mais
             no <span>Back-End</span>. Eu quero me tornar Full-Stack no futuro e
             fazer apps mobiles também.
+            </div>
+
+              
           </div>
           <div className="arrows">
-            <div className="SmallArrow"><KeyboardArrowDownRoundedIcon/> </div>
-            <div className="BigArrow"> <ArrowForwardIosRoundedIcon/> </div>
+            <div className="smallArrow"><KeyboardArrowDownRoundedIcon sx={{fontSize:55}} /> </div>
+            <div className="bigArrow"> <KeyboardArrowDownRoundedIcon sx={{fontSize:70}} /> </div>
           </div>
         </div>
       </section>
