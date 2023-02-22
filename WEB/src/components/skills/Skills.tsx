67d424b0222e73f48a196b724c5ec109e6e6205b
Nickolas <89612417+lothllann@users.noreply.github.com>
2023-02-22 17:09:01 -0300

    skills

--- WEB/src/components/skills/Skills.tsx
+++ WEB/src/components/skills/Skills.tsx
@@ -2,8 +2,35 @@ import React from "react";
 import Head from "../helper/Head";
 import "./Skills.css";
 import "../../App.css";
-import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded'; //seta menor para baixo
+import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded"; //seta menor para baixo
 
+const languagesInfo = [
+  {
+    title: "CSS",
+    image: "",
+    percentage: 28,
+  },
+  {
+    title: "JavaScript",
+    image: "",
+    percentage: 24.14,
+  },
+  {
+    title: "HTML",
+    image: "",
+    percentage: 21.8,
+  },
+  {
+    title: "Java",
+    image: "",
+    percentage: 15.44,
+  },
+  {
+    title: "TypeScript",
+    image: "",
+    percentage: 10.62,
+  },
+];
 
 const Skills = () => {
   return (
@@ -15,25 +42,36 @@ const Skills = () => {
       <section className="mainSkills">
         <div className="introSkills">
           <div className="apresentacao">
-          <h1 className="text-4xl font-extrabold mb-5">Skills</h1>
+            <h1 className="text-4xl font-extrabold mb-5">Skills</h1>
 
             <div>
-            Como eu curto mais o <span>Front-End</span> as minhas habilidades
-            estão mais concentradas nessa area, porém eu pretendo evoluir mais
-            no <span>Back-End</span>.<br/> Eu quero me tornar <span>Full-Stack</span> no futuro e
-            fazer apps mobiles também.
+              Como eu curto mais o <span>Front-End</span> as minhas habilidades
+              estão mais concentradas nessa area, porém eu pretendo evoluir mais
+              no <span>Back-End</span>.<br /> Eu quero me tornar
+              <span> Full-Stack</span> no futuro e fazer apps mobiles também.
             </div>
-
-              
           </div>
           <div className="arrows">
-            <div className="smallArrow"><KeyboardArrowDownRoundedIcon sx={{fontSize:55}} /> </div>
-            <div className="bigArrow"> <KeyboardArrowDownRoundedIcon sx={{fontSize:70}} /> </div>
+            <div className="smallArrow">
+              <KeyboardArrowDownRoundedIcon sx={{ fontSize: 55 }} />
+            </div>
+            <div className="bigArrow">
+              <KeyboardArrowDownRoundedIcon sx={{ fontSize: 70 }} />
+            </div>
           </div>
         </div>
 
-        <div className="Linguagens">
-          <h1 className="text-4xl font-extrabold mb-5">Linguagens de Programação</h1>
+        <div className="linguagensSkills">
+          <h1 className="text-4xl font-extrabold mb-5">
+            Linguagens de Programação
+          </h1>
+          <div className="cardsContainer--Linguagens">
+            {languagesInfo.map((language,i)=>{
+              return(
+                <div key={i} className='card--language'>{language.title}</div>
+              )
+            })}
+          </div>
         </div>
 
         <div className="tecnologias">
