02b81b3e2945911f04671de0b41ce0e855ce2b77
Nickolas <89612417+lothllann@users.noreply.github.com>
2023-03-15 23:38:14 -0300

    ....

--- src/components/sobre/Sobre.tsx
+++ src/components/sobre/Sobre.tsx
@@ -21,65 +21,63 @@ const Sobre = () => {
     <main className="mainAbout">
       <Head title="About" description="página que fala um pouco sobre mim" />
 
+      <section className="about">
+        <h1 className="text-4xl font-extrabold mb-5">Sobre Mim</h1>
+        <div>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </div>
+      </section>
 
-        <section className="about">
-          <h1 className="text-4xl font-extrabold mb-5">Sobre Mim</h1>
-          <div>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+      <section className={`${open ? "active " : ""}card`}>
+        <div className="user">
+          <div className="img">
+            <img src={photo} alt="minha foto de perfil" />
           </div>
-        </section>
-
-        <section className={`${open ? "active " : ""}card`}>
-          <div className="user">
-            <div className="img">
-              <img src={photo} alt="minha foto de perfil" />
-            </div>
-            <div className="content">
-              <h2>
-                Nickolas Ruppenthal
-                <br />
-                <span>Web Developer</span>
-              </h2>
-            </div>
-            <button className="toggle" onClick={handleToggle}>{`${
-              open ? "Close" : "Click Here"
-            }`}</button>
+          <div className="content">
+            <h2>
+              Nickolas Ruppenthal
+              <br />
+              <span>Web Developer</span>
+            </h2>
           </div>
+          <button className="toggle" onClick={handleToggle}>{`${
+            open ? "Close" : "Click Here"
+          }`}</button>
+        </div>
 
-          {open ? (
-            <div className="containerSocialMedia">
-              {listOfSocialMedias.map((socialMedia, i) => (
-                <a
-                  key={`${socialMedia.name}-${i}`}
-                  href={socialMedia.link}
-                  className="socialMediaCard"
-                >
-                  <div className="sociallMediaImg">{socialMedia.image}</div>
-                  <span
-                    data-textColor={socialMedia.name}
-                    className="sociallMediaText 
+        {open ? (
+          <div className="containerSocialMedia">
+            {listOfSocialMedias.map((socialMedia, i) => (
+              <a
+                key={`${socialMedia.name}-${i}`}
+                href={socialMedia.link}
+                className="socialMediaCard"
+              >
+                <div className="sociallMediaImg">{socialMedia.image}</div>
+                <span
+                  data-textColor={socialMedia.name}
+                  className="sociallMediaText 
                    data-[textColor=GitHub]:text-GitHub 
                    data-[textColor=Linkedin]:text-Linkedin 
                    data-[textColor=Instagram]:text-Instagram 
                    data-[textColor=WhatsApp]:text-WhatsApp 
                    data-[textColor=Gmail]:text-Gmail"
-                  >
-                    {socialMedia.name}
-                  </span>
-                </a>
-              ))}
-            </div>
-          ) : null}
-        </section>
-
+                >
+                  {socialMedia.name}
+                </span>
+              </a>
+            ))}
+          </div>
+        ) : null}
+      </section>
 
       <div className="absolute bottom-[0px] translate-x-[-250px] opacity-0 animate-slideRight">
         <Lottie animationData={coffe} style={style} />
