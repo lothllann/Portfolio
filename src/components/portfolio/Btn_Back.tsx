62694cd3a353643d675a9c48c8372c580ece3f9f
Nickolas <89612417+lothllann@users.noreply.github.com>
2023-03-16 22:28:48 -0300

    front-end

--- src/components/portfolio/Btn_Back.tsx
+++ src/components/portfolio/Btn_Back.tsx
@@ -2,13 +2,16 @@ import { Link } from 'react-router-dom'
 import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
 import grey from '@mui/material/colors/grey';
 
-
+const style = {
+  color: grey,
+  fontSize: 0.06 * window.innerHeight
+}
 
 const Btn_Back = () => {
   return (
     <Link to='/port'>
-        <button className="absolute left-0 top-0 w-[75px] h-[75px] shadow-[0_5px_15px_rgba(0,0,0,0.2)]  bg-greyGlass1 backdrop-blur-lg  rounded-[50%] border-greyGlass2 border-[1px] border-solid animate-slideUpAndFade transition-all ease-out hover:bg-ligth_green"> 
-        <ArrowBackRoundedIcon sx={{color: grey, fontSize:50 }}/>
+        <button className="absolute left-[-25px] max-[1400px]:top-[-25px] top-[-50px] w-[4.5%] h-[9%] shadow-[0_5px_15px_rgba(0,0,0,0.2)]  bg-greyGlass1 backdrop-blur-lg  rounded-[50%] border-greyGlass2 border-[1px] border-solid animate-slideUpAndFade transition-all ease-out hover:bg-ligth_green"> 
+        <ArrowBackRoundedIcon sx={style}/>
         </button>
     </Link>
 
