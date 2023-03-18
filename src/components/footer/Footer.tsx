6b2289b496e77fa7685a5765a6e54ba816b1e152
lothllann <nick.ruppenthal@gmail.com>
2023-03-17 21:35:24 -0300

    footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,23 @@
+import falcon from '../../assets/millennium-falcon.png'
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+
+const Footer = () => {
+  return (
+    <footer className='mt-[200px] flex flex-col items-center'>
+      <img className='w-[50px] mb-[35px]' src={falcon} alt="separador do footer" />
+      <h1 className='font-bold'>Sigma-me</h1>
+      <div className='flex gap-[10px] mt-[15px]'>
+        <div className='cursor-pointer hover:scale-125 transition-all'> <a href="https://www.instagram.com/ruupp___/"><InstagramIcon sx={{ fontSize: 40, color: "#E4405F" }}/> </a>  </div>
+        <div className='cursor-pointer hover:scale-125 transition-all'> <a href="https://github.com/lothllann"><GitHubIcon sx={{ fontSize: 40, color: "#BABBBD" }} /> </a>  </div>
+        <div className='cursor-pointer hover:scale-125 transition-all'> <a href="https://www.linkedin.com/in/nickolas-ruppenthal-38b2ba167/"><LinkedInIcon sx={{ fontSize: 40, color: "#0A66C2" }}/> </a> </div>
+      </div>
+      <div className='mb-[15px] mt-[20px] italic text-[12px]'>
+       ©2023 - Nickolas B. Ruppenthal 
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/portfolio/Front_End.tsx
+++ src/components/portfolio/Front_End.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { site } from '../../utils/sites'
 import SiteCard from '../card/SiteCard'
+import Footer from '../footer/Footer'
 import Btn_Back from './Btn_Back'
 
 export const Front_End = () => {
   return (
-    <section className='h-[100vh]'>
+    <section className='container_Front'>
       <Btn_Back/>
-      <h1 className="text-4xl font-extrabold mb-10">Front-End</h1>
+      <h1 className="text-4xl font-extrabold mb-[75px]">Front-End</h1>
+      <main className='mainFront-end single'>
       {site.map((i, index)=>(
         <SiteCard key={index} description={i.description} title={i.titulo} srcImg={i.srcImg} featured={i.srcImgFeatured}/>
       ))}
+      </main>
+
+        <Footer/>
       
     </section>
   )
