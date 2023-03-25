c85b94d8f07aa3f61c49bebe19311497205fb608
lothllann <nick.ruppenthal@gmail.com>
2023-03-24 21:42:29 -0300

    .

--- src/components/modalCard/ModalCard.tsx
+++ src/components/modalCard/ModalCard.tsx
@@ -12,10 +12,7 @@ interface ModalProps {
   how: string;
   title: string
 }
-const style = {
-  color: grey,
-  fontSize: 35
-}
+
 const ModalCard = ({ isOpen, setOpenModal, how, tecs, langs, title }: ModalProps) => {
   let filtrado;
   const tecsFilter = tecs.map((t)=>(
@@ -27,7 +24,7 @@ const ModalCard = ({ isOpen, setOpenModal, how, tecs, langs, title }: ModalProps
   <div className="fixed right-0 flex items-end justify-end w-[100%] h-[100vh] z-50">
     <div className="p-[25px] relative w-[45%] h-[80%] flex-col backdrop-blur-sm bg-greyGlass4 border-greyGlass2 border-[1px] border-solid rounded-lg flex shadow-[0_5px_15px_rgba(0,0,0,0.2)]">
       <button onClick={() => setOpenModal(false)} className="flex items-center justify-center w-[45px] h-[45px] absolute top-[-15px] left-[-15px] bg-[#323232] border-[1px] border-solid rounded-full border-greyGlass2 hover:bg-[#ff0000] hover:scale-110 transition-all">
-      <CloseRoundedIcon style={style}/>
+      <CloseRoundedIcon sx={{color: grey, fontSize: 35}}/>
       </button>
 
       <div className="flex flex-1 items-start justify-center flex-col">
