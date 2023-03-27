90b482e90f980b4bfb56d77e45e1b9dd38c7db69
lothllann <nick.ruppenthal@gmail.com>
2023-03-27 14:59:08 -0300

    modal

---
+++ src/components/graficos/Pizza.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pizza = () => {
+  return (
+    <div className='absolute right-0 '> Grafico de Pizza</div>
+  )
+}
+
+export default Pizza
--- src/components/modalCard/ModalCard.tsx
+++ src/components/modalCard/ModalCard.tsx
@@ -3,6 +3,7 @@ import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import { grey } from "@mui/material/colors";
 import { all } from "../../utils/tecnologias";
 import HoverCardElement from "../card/HoverCard";
+import Pizza from "../graficos/Pizza";
 
 interface ModalProps {
   isOpen: boolean;
@@ -21,16 +22,17 @@ const ModalCard = ({ isOpen, setOpenModal, how, tecs, langs, title }: ModalProps
 
  
   if (isOpen) return (
-    <div className="flex fixed right-[-100px] p-[25px]  w-[45%]  flex-col backdrop-blur-sm bg-greyGlass4 border-greyGlass2 border-[1px] border-solid rounded-lg  shadow-[0_5px_15px_rgba(0,0,0,0.2)]">
-      <button onClick={() => setOpenModal(false)} className="flex items-center justify-center w-[45px] h-[45px] absolute top-[-15px] left-[-15px] bg-[#323232] border-[1px] border-solid rounded-full border-greyGlass2 hover:bg-[#ff0000] hover:scale-110 transition-all">
+    <div className="z-50 flex fixed top-0 right-[-100px] p-[25px]  w-[40%] h-[100vh]  flex-col backdrop-blur-sm bg-greyGlass4  rounded-lg  shadow-[-15px_15px_15px_rgba(0,0,0,0.2)]">
+      <button onClick={() => setOpenModal(false)} className="flex items-center justify-center w-[45px] h-[45px] absolute top-[55px] left-[-23px] bg-[#323232] border-[1px] border-solid rounded-full border-greyGlass2 hover:bg-[#ff0000] hover:scale-110 transition-all">
       <CloseRoundedIcon sx={{color: grey, fontSize: 35}}/>
       </button>
+      <Pizza/>
 
       <div className="flex flex-1 items-start justify-center flex-col">
-        <h1 className="text-4xl font-extrabold ">{title}</h1>
+        <h1 className="text-4xl font-extrabold mb-[50px] ">{title}</h1>
         
-        <div className="w-[75%]">
-        <h2>Como eu fiz?</h2>
+        <div className="w-[75%] mb-[100px]">
+        <h2 className="text-2xl font-bold mb-[10px] mt-[25px]">Como eu fiz?</h2>
           {how}
         </div>
       </div>
@@ -45,7 +47,7 @@ const ModalCard = ({ isOpen, setOpenModal, how, tecs, langs, title }: ModalProps
             srcImg={tf[0].srcImg}
             about={tf[0].about}
             socialMedia={tf[0].socialMedia}
-            link={tf[0].link}
+            link={tf[0].link} 
           />
           ))}
         </div>
